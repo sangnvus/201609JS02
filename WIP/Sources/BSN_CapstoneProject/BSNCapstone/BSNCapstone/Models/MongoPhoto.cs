ed5a0c62e3e0e54429ecc1b1f72faf3d287f0093
HuyenPTSE03416 <huyenptse03416@fpt.edu.vn>
2016-10-31 23:36:06 +0700

    HuyenPT: Commit model for Image Upload

---
+++ WIP/Sources/BSN_CapstoneProject/BSNCapstone/BSNCapstone/Models/MongoPhoto.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BSNCapstone.Models
+{
+    public class MongoPhoto
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public String Id { get; set; }
+
+        [Required]
+        public string PublicId { get; set; }
+
+        public string Desc { get; set; }
+    }
+}
