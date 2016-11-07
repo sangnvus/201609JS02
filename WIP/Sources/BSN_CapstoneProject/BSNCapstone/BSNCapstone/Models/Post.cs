df9dfc715737d1a836f9fda6e388e704a04f01a6
HuyenPTSE03416 <huyenptse03416@fpt.edu.vn>
2016-11-08 01:39:36 +0700

    [Coding]_Commit source code for function POST Note: Source chưa đầy đủ (chưa áp dụng thời gian thực), commit tạm thời để bên design fix lỗi giao diện khi thêm dữ liệu thật.

---
+++ WIP/Sources/BSN_CapstoneProject/BSNCapstone/BSNCapstone/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSNCapstone.Models
+{
+    public class Post
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserID { get; set; }
+
+        public string GroupId { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string BookId { get; set; }
+
+        public string Content { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
