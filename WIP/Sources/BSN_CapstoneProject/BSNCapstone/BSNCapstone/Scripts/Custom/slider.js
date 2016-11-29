eb261a52d147a98cbc7ac694120520d69eed00b0
HuyenPTSE03416 <huyenptse03416@fpt.edu.vn>
2016-11-30 02:16:45 +0700

    [Coding]_Tách các đoạn script ra file riêng Đã tách ở phần slider và publisher

---
+++ WIP/Sources/BSN_CapstoneProject/BSNCapstone/BSNCapstone/Scripts/Custom/slider.js
@@ -0,0 +1,52 @@
+﻿
+
+function Upload() {
+    var formData = new FormData();
+    var totalFiles = document.getElementById("slide").files.length;
+    for (var i = 0; i < totalFiles; i++) {
+        var file = document.getElementById("slide").files[i];
+        formData.append("slide", file);
+    }
+    formData.append("desc", $("#description").val());
+    $.ajax({
+        type: "POST",
+        url: "Upload/Slider",
+        data: formData,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+
+        success: function (result) {
+            alert(result);
+            window.location.reload();
+        },
+        error: function (err) {
+            alert(err.statusText);
+        }
+    });
+}
+
+
+function Delete(id, i) {
+    var deleteConfirm = confirm("Bạn có chắc chắn muốn xóa slide này không? ");
+    if (deleteConfirm) {
+        var formData = new FormData();
+        formData.append("id", id);
+        $.ajax({
+            type: "POST",
+            url: "DeleteConfirmed/Slider",
+            data: formData,
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+
+            success: function (result) {
+                alert(result);
+                $('table tbody tr#' + i).fadeOut('slow', function () { $(this).remove(); })
+            },
+            error: function (err) {
+                alert(err.statusText);
+            }
+        })
+    }
+}
